09ffe59a85db2d9058e07b334e5d1f70e3661408
lenatheque <lbalvarez8@gmail.com>
2021-01-21 11:26:51 -0500

    adding shake effect to the ball

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
-	var magic8Ball ={};
-magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams"];
-
-magic8Ball.askQuestion=
-	function(question){
-	var randomNumber=
-		Math.random();
-	var randomNumberForListOfAnswers= randomNumber * this.listOfAnswers.length;
-var randomIndex = Math.floor(randomNumberForListOfAnswers);
-	var answer= this.listOfAnswers [randomIndex];
-	console.log(question);
-	console.log(answer);
-};
-magic8Ball.askQuestion("Will I today be a great day?");
-});
-
-var onClick = function() {
  
-    var question = prompt("ASK A YES/NO QUESTION!");
-    magic8Ball.askQuestion(question);
-};
-$("#questionButton").click( onClick );
+    var magic8Ball = {};
+    	
+$("#answer").hide();
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+ 
+    magic8Ball.askQuestion = function(question){
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+
+        $("#answer").text( answer );
+ 
+        console.log(question);
+        console.log(answer);
+			
+$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
+    };
+
+    var onClick = function() {
+        var question = prompt("ASK A YES/NO QUESTION!");
+        magic8Ball.askQuestion(question);
+    };
+$("#answer").hide();
+    $("#questionButton").click( onClick );
+ $("#answer").hide();
+	$("#8ball").effect( "shake" );
+ 
+$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+ 
+//wait half a second before showing prompt
+   setTimeout(
+       function() {
+           //show prompt
+           var question = prompt("Ask a yes or no question")
+           magic8Ball.askQuestion(question)
+       }, 500);
+});
