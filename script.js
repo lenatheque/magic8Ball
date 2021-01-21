fb70d3397a592ddcd7adf78c81fdb963eff30334
lenatheque <lbalvarez8@gmail.com>
2021-01-21 13:39:34 -0500

    styling the text

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
-$(document).ready(function(){
-  
-var magic8ball = {};
-  
-magic8ball.listOfAnswers = ["Yes", "No", "Maybe", "Ask again later", "In your dreams", "It is certain", "Why not?", "I have no clue"];
+$(document).ready(function() {
+  var magic8Ball = {};
 
-// define the method
- magic8ball.askQuestion= function(question) { 
-   
-   // create a random number
-      var randomNumber = Math.random();
-      var randomAnswer = Math.floor(randomNumber * this.listOfAnswers.length);
-    var randomIndex = Math.floor(randomAnswer);
-      var answer = this.listOfAnswers[randomIndex];
- 
-      $("#answer").hide();
-      $("#8ball").effect( "shake");   
-      
-      $("#answer").fadeIn(4000);
- $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
-      $("#answer").text( answer );
-   
-   console.log(question);
-   console.log(answer);
-}; 
-  var onClick = function() {  
-        
-        	
+  magic8Ball.listOfAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes ~ definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+
+  $("#answer").hide();
+
+  var onClick = function() {
     $("#answer").hide();
-    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
 
-    //wait half a second before showing prompt
-setTimeout(function() {
-//show prompt
+    $("#eightball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
 
-      var question = prompt("Ask a yes or no question");
+    setTimeout( function() {
+      var question = prompt("ASK A YES/NO QUESTION!");
+      $("#eightball").effect("shake");
 
-           magic8ball.askQuestion(question);
+      magic8Ball.askMagic8Ball(question);
 
-       }, 500);
-  
+    }, 500);
   };
-        $("#questionButton").click(onClick);
-  
+
+  magic8Ball.askMagic8Ball = function(question) {
+    $("#eightball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+
+    $("#answer").fadeIn(4000);
+
+    var randomNumber = Math.random() * this.listOfAnswers.length;
+    var randomIndex = Math.floor(randomNumber);
+
+    $("#answer").text(this.listOfAnswers[randomIndex] + ".");
+  };
+
+  $("#questionButton").click(onClick);
+});
